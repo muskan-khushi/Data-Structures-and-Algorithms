250094f37b84bf44cd003ff57e6063b4f56fda2b
Muskan <muskankhushi2022@gmail.com>
2025-03-29 09:33:36 +0530

    .

--- Tree/5.cpp
+++ Tree/5.cpp
@@ -95,3 +95,43 @@ class Solution
        return ans;
     }
 };
+
+//top view of binary tree
+void find(Node* root, int pos, int& l, int& r){
+    if(root == NULL) return;
+    l=min(l,pos);
+    r = max(r,pos);
+    find(root->left,-1,l,r);
+    find(root->right,1,l,r);
+}
+
+vector<int> topView(Node* root){
+    int l = 0, r = 0;
+    find(root, 0, l, r);
+    vector<int> ans(r-l+1);
+    vector<bool>filled(r-l+1);
+    queue<Node*>q;
+    queue<int>index;
+    q.push(root);
+    index.push(-1*l);
+    while(!q.empty()){
+        Node* temp = q.front();
+        q.pop();
+        int pos = index.front();
+        index.pop();
+        if(!filled[pos]){
+            filled[pos] = 1;
+            ans[pos] = temp->data;
+        }
+        if(temp->left) {
+            q.push(temp->left);
+            index.push(pos-1);
+        }
+        if(temp->right){
+            q.push(temp->right);
+            index.push(pos+1);
+        }
+    }
+    return ans;
+}
+
